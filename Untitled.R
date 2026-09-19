c6d854331bf257902b6d50c0f6f8077a66e0f842
garzajesh-glitch <garzajesh@gmail.com>
2026-09-18 19:21:15 -0600

    Add files via upload

--- Untitled.R
+++ Untitled.R
@@ -1,32 +1,18 @@
-# 1. Cargar la librería para análisis de datos
-# install.packages("tidyverse")
 
-library(tidyverse)
-autos <- mpg
-# 2. R ya incluye varios datasets de prueba.
-# Usaremos 'mpg', que contiene datos de economía de combustible de 38 modelos de autos.
-# Vamos a ver las primeras filas en la consola:
-print(head(autos))
+library( dplyr)
+library(ggplot2)
 
-# 3. MANIPULACIÓN DE DATOS (Filtrar y Seleccionar)
-# Queremos ver solo los autos modernos (año 2008) y con motores grandes (cilindrada > 4 litros)
-autos_grandes <- autos %>%
-  filter(year == 2008, displ > 4)
+df <- read.csv("d3_aire01_49_1.csv")
+df <- df[!is.na(df$NOx), ]
 
-# Mostramos el resultado filtrado
-print("Autos del 2008 con motores grandes:")
-print(autos_grandes)
+top10 <- df %>%
+  group_by(Entidad) %>%
+  summarise(NOx = sum(NOx), .groups = 'drop') %>%
+  arrange(desc(NOx))%>%
+  head(10)
 
-# 4. VISUALIZACIÓN DE DATOS (Crear una gráfica)
-# Vamos a graficar el tamaño del motor (displ) contra el consumo en autopista (hwy)
-grafica <- ggplot(data = autos) +
-  geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
-  labs(
-    title = "Tamaño del Motor vs Consumo en Autopista",
-    x = "Tamaño del Motor (Litros)",
-    y = "Millas por Galón en Autopista"
-  ) +
-  theme_minimal()
+ggplot(top10, aes(x = reorder(Entidad, NOx), y = NOx)) + geom_bar(stat = 'identity', fill = 'gray', color = 'black',linewidth = 0.8)+
+  labs(x = 'Entidad Federativa', y = 'Emisiones de NOx (Toneladas)') +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
 
-# Mostrar la gráfica en PyCharm
-print(grafica)
